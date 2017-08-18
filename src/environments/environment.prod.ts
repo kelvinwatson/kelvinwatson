6c9387733d4424eae967409404daf09710f92f39
Watson,Kelvin <kelvincswatson@gmail.com>
2017-08-17 20:42:14 -0400

    layout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPY4upYrG7KTASEOS8McUXgZii4Mb0Gr4",
+    authDomain: "kelvinwatson-53204.firebaseapp.com",
+    databaseURL: "https://kelvinwatson-53204.firebaseio.com",
+    projectId: "kelvinwatson-53204",
+    storageBucket: "",
+    messagingSenderId: "565863015502"
+  }
 };
