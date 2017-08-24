515090ff79a94c47a7c75a57b1739b30459eec70
Watson,Kelvin <kelvincswatson@gmail.com>
2017-08-23 20:47:20 -0400

    overlay animation

--- src/components/InformationOverlay/InformationOverlay.js
+++ src/components/InformationOverlay/InformationOverlay.js
@@ -4,9 +4,21 @@ import './InformationOverlay.css'
 class InformationOverlay extends Component {
   render(){
     return (
-      <div className={`information-overlay-container ${this.props.isOpenOverlay ? 'information-overlay-container-open' : ''}`}>
-        <div onClick={this.props.onCloseInfoOverlayClicked}> X </div>
-        <h1>I am InformationOverlay!!!</h1>
+      <div className={`information-overlay-container ${this.props.isOpenOverlay ? 'information-overlay-container-open information-overlay-container-animation' : ''}`}>
+        <div className="content-wrapper">
+          <div onClick={this.props.onCloseInfoOverlayClicked}> X </div>
+
+          <h1>I am Kelvin Watson</h1>
+
+          <div className="information-overlay-text-flex">
+            <div className="information-overlay-text-flex-item">
+              <p>In my past life, I was a hospital pharmacist.</p>
+              </div>
+            <div className="information-overlay-text-flex-item">
+              <p>Right</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
