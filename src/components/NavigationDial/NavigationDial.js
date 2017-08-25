feb32710db1fa812cfc20e84396751a072fd4cf7
Watson,Kelvin <kelvincswatson@gmail.com>
2017-08-24 23:08:29 -0400

    nav

--- src/components/NavigationDial/NavigationDial.js
+++ src/components/NavigationDial/NavigationDial.js
@@ -5,57 +5,39 @@ class NavigationDial extends Component {
   render(){
     return (
       <nav>
-
         <ul id="navigationDial">
-
-
-          <div className="quadrant" id="quadrant1"></div>
-          <div className="quadrant" id="quadrant2"></div>
-          <div className="quadrant" id="quadrant3"></div>
-          <div className="quadrant" id="quadrant4"></div>
+          <div className="quadrant" id="quadrant1">
+            <div className="quadrant-text quadrant-text-1">HIRE</div>
+          </div>
+          <div className="quadrant" id="quadrant2">
+            <div className="quadrant-text quadrant-text-2">PROJECTS</div>
+          </div>
+          <div className="quadrant" id="quadrant3">
+            <div className="quadrant-text quadrant-text-3">STACK</div>
+          </div>
+          <div className="quadrant" id="quadrant4">
+            <div className="quadrant-text quadrant-text-4">ME</div>
+          </div>
           <li id="rotator1"></li>
           <li id="rotator2"></li>
           <li id="rotator3"></li>
           <li id="rotator4"></li>
-          {/*<div className="quadrant" id="quadrant1"></div>*/}
 
+          <div className="line line-black line1"></div>
+          <div className="line line-black line1a"></div>
+
+          <div className="line line-black line2"></div>
+          <div className="line line-black line2a"></div>
+
+
+
+          <div className="line line-black line3"></div>
+          <div className="line line-black line3a"></div>
+
+         <div className="line line-black line4"></div>
+         <div className="line line-black line4a"></div>
 
         </ul>
-        {/*}<div className="ring">
-          <div className="led-container-projects">
-            <div className="led-projects"></div>
-            <a id="led-overlay-projects" href="#projectsSection">
-              <div id="nav-hover-title-projects">PROJECTS</div>
-            </a>
-          </div>
-
-          <div className="led-container-tech">
-            <div className="led-tech"></div>
-            <a id="led-overlay-tech" href="#technologiesSection">
-              <div id="nav-hover-title-tech">TECH STACK</div>
-            </a>
-          </div>
-
-          <div className="led-container-hire">
-            <div className="led-hire"></div>
-            <a id="led-overlay-hire" href="#hireSection">
-              <div id="nav-hover-title-hire">HIRE ME</div>
-            </a>
-          </div>
-
-          <div className="led-container-career">
-            <div className="led-career"></div>
-            <a id="led-overlay-career" href="#hireSection">
-              <div id="nav-hover-title-career">CAREER</div>
-            </a>
-          </div>
-
-          <div className="inner-glow-career"></div>
-          <div className="inner-glow-hire"></div>
-
-          <div className="face"></div>
-        </div>*/}
-
       </nav>
     );
   }
